a71c9f6eb126a5fd2c97562a2ad39b1f88a7c46c
Crystal Lim <crystalcrystallim@gmail.com>
2021-01-20 16:13:00 +1300

    created item components for home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import './App.css';
 import Header from './components/Header';
+import Item from './components/Item';
+
+import Poster from './assets/poster.jpg'
+import Safety from './assets/safety.jpg'
+
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className='app__itemsContainer'>
+        <Item
+        title='title one'
+        desc='description'
+        descLink=''
+        backgroundImg={Poster}
+        leftBtnTxt='order now'
+        leftBtnLink=''
+        rightBtnTxt='learn more'
+        rightBtnLink=''
+        twoButtons='true'
+        first
+        />
+        <Item        
+          title='title two'
+          desc='description'
+          descLink=''
+          backgroundImg={Safety}
+          leftBtnTxt='order now'
+          leftBtnLink=''
+          rightBtnTxt='learn more'
+          rightBtnLink=''
+          twoButtons='true'
+          />
+      </div>
     </div>
   );
 }
